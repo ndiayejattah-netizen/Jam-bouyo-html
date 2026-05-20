2d630f513a4ce86969459fff10f918aba36fee3e
ndiayejattah-netizen <ndiayejattah@gmail.com>
2026-05-20 03:13:17 +0200

    Update send-admission-email.js

--- api/send-admission-email.js
+++ api/send-admission-email.js
@@ -2,7 +2,6 @@ export default async function handler(req, res) {
   if (req.method !== "POST") {
     return res.status(405).json({ error: "Method not allowed" });
   }
-
   try {
     const data = req.body;
 
